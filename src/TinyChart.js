a9088c13fd0d6fa600abbdd48ce936fa33171ae8
badwiso <amaramontassar@formanet-tn.com>
2022-04-01 11:11:24 +0100

    chart

--- src/TinyChart.js
+++ src/TinyChart.js
@@ -1,42 +1,28 @@
-import {Chart,} from 'chart.js';
+
+import {Chart, ArcElement} from 'chart.js'
+import { Doughnut } from 'react-chartjs-2';
+Chart.register(ArcElement);
 function TinyChart(){
-    const ctx = 'myChart';
-    const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+    const data = {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 205, 86)'
+    ],
+    spacing:2,
+    hoverOffset: 4
+  }]
+};
     return(
-        <canvas id={ctx} width="400" height="400"></canvas>
+        
+<Doughnut data={data} style={{width:"56px"}} />
     );
 }
 export default TinyChart;
